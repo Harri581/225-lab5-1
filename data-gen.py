67b502125476ec83c96412a94718965eebc85cb1
Harri581 <162341101+Harri581@users.noreply.github.com>
2025-05-15 23:10:38 -0400

    Update data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -13,6 +13,7 @@ def generate_test_data(num_contacts):
     for i in range(num_contacts):
         name = f'Test Name {i}'
         phone = f'123-456-789{i}'
+        age = f'21{i}'
         db.execute('INSERT INTO contacts (name, phone) VALUES (?, ?)', (name, phone))
     db.commit()
     print(f'{num_contacts} test contacts added to the database.')
